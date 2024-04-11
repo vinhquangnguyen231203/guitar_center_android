cfab9a9796e5ddb1851455369daf722a90823c66
vinhquangnguyen231203 <vinhquangnguyennn@gmail.com>
2024-04-11 21:26:32 +0700

    push model _ OrderRequest

--- app/src/main/java/com/example/guitar_center_android/Domain/model/OrderRequest.java
+++ app/src/main/java/com/example/guitar_center_android/Domain/model/OrderRequest.java
@@ -1,10 +1,30 @@
 package com.example.guitar_center_android.Domain.model;
 
+import com.squareup.moshi.Json;
+
 import java.util.List;
 
 public class OrderRequest {
+    @Json(name = "order")
     private Order order;
-    private List<Order> orderDetails;
 
+    @Json(name = "orderDetails")
+    private List<OrderDetail> orderDetails;
 
+    // Getters and setters
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public List<OrderDetail> getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(List<OrderDetail> orderDetails) {
+        this.orderDetails = orderDetails;
+    }
 }

995896a5ce896250ad16879911f4b5416b3e9b45
vinhquangnguyen231203 <vinhquangnguyennn@gmail.com>
2024-04-11 19:27:04 +0700

    Update tree project

--- app/src/main/java/com/example/guitar_center_android/Presentation/Activity/MainActivity.java
+++ app/src/main/java/com/example/guitar_center_android/Presentation/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.guitar_center_android;
+package com.example.guitar_center_android.Presentation.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.guitar_center_android.R;
 
 public class MainActivity extends AppCompatActivity {
 

20c9ba04505181080ef7efb5d1cb710af2d72ead
vinhquangnguyen231203 <vinhquangnguyennn@gmail.com>
2024-04-13 18:26:29 +0700

    update Cart Activity

---
+++ app/src/main/java/com/example/guitar_center_android/Presentation/Activity/CartActivity.java
@@ -0,0 +1,16 @@
+package com.example.guitar_center_android.Presentation.Activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.guitar_center_android.R;
+
+public class CartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment_cart);
+    }
+}

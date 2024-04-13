5f13a3447f09b23632508ae16efd0282f77ead17
vinhquangnguyen231203 <vinhquangnguyennn@gmail.com>
2024-04-13 11:07:38 +0700

    add file Activity

--- app/src/main/java/com/example/guitar_center_android/Presentation/Activity/DetailsActivity.java
+++ app/src/main/java/com/example/guitar_center_android/Presentation/Activity/DetailsActivity.java
@@ -1,4 +1,16 @@
 package com.example.guitar_center_android.Presentation.Activity;
 
-public class DetailsActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.guitar_center_android.R;
+
+public class DetailsActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_details);
+
+    }
 }
--- app/src/main/java/com/example/guitar_center_android/Presentation/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/guitar_center_android/Presentation/Activity/RegisterActivity.java
@@ -1,4 +1,16 @@
 package com.example.guitar_center_android.Presentation.Activity;
 
-public class RegisterActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.guitar_center_android.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
 }

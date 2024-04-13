17ca0b1c91b6b14335b107188a5e24fdb442c4e3
tientm2610 <t.manh.tien2610@gmail.com>
2024-04-13 11:05:19 +0700

    complete Home_List_Adapter

--- app/src/main/java/com/example/guitar_center_android/Presentation/Activity/MainActivity.java
+++ app/src/main/java/com/example/guitar_center_android/Presentation/Activity/MainActivity.java
@@ -4,15 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.guitar_center_android.Presentation.Adapter.Home_List_Adapter;
 import com.example.guitar_center_android.R;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Đây là MainActivity
-        //
+        Home_List_Adapter homeListAdapter = new Home_List_Adapter(this);
     }
 }

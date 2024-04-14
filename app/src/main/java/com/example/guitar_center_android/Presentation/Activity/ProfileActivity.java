f6cdb7af4a8f82dda05b5133a91f3803ed8ad02f
tientm2610 <t.manh.tien2610@gmail.com>
2024-04-14 17:51:18 +0700

    complete Profile_Adapter

--- app/src/main/java/com/example/guitar_center_android/Presentation/Activity/ProfileActivity.java
+++ app/src/main/java/com/example/guitar_center_android/Presentation/Activity/ProfileActivity.java
@@ -1,6 +1,10 @@
 package com.example.guitar_center_android.Presentation.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +16,8 @@ import com.example.guitar_center_android.R;
 public class ProfileActivity extends AppCompatActivity {
     private  UserManager userManager;
     private Profile_Adapter profileAdapter;
-
+    private Button btnUpdateProfile ;
+    private ImageView btnBackToHome;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +28,28 @@ public class ProfileActivity extends AppCompatActivity {
         // Gọi hàm getUserInfor từ Profile_Adapter
         profileAdapter.getUserInfor();
 
+        //Lấy id của các button
+    btnUpdateProfile = findViewById(R.id.btn_updateProfile);
+    btnBackToHome = findViewById(R.id.btn_profile_toHome);
+
+    //Cập nhật thông tin khi nhấn vào nút cập nhật
+    btnUpdateProfile.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //cập nhật thông tin người dùng
+            profileAdapter.updateUserInfor();
+
+            //load lại thông tin người dùng sau khi cập nhật
+            profileAdapter.getUserInfor();
+        }
+    });
+    // quay trở về trang home
+    btnBackToHome.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    });
     }
 }
--- app/src/main/java/com/example/guitar_center_android/Presentation/Adapter/Profile_Adapter.java
+++ app/src/main/java/com/example/guitar_center_android/Presentation/Adapter/Profile_Adapter.java
@@ -13,6 +13,8 @@ import com.example.guitar_center_android.Presentation.Activity.ProfileActivity;
 import com.example.guitar_center_android.R;
 import com.google.gson.Gson;
 
+import java.text.ParseException;
+import java.util.Date;
 import java.util.Locale;
 
 import retrofit2.Call;
@@ -22,15 +24,17 @@ import retrofit2.Response;
 public class Profile_Adapter {
     private Context context;
     private UserManager userManager;
-    private EditText textViewFullname, textViewPassword, textViewPhone, textViewBirth, textViewAddress;
+    private EditText textViewUsername,textViewFullname, textViewPassword, textViewPhone, textViewBirth, textViewAddress;
 
     public  Profile_Adapter(Context context, UserManager userManager){
         this.context = context;
         this.userManager = userManager;
     }
+
     public void getUserInfor(){
 
         //lay id giao dien
+        textViewUsername = ((ProfileActivity) context).findViewById(R.id.txt_profile_username);
         textViewFullname = ((ProfileActivity) context).findViewById(R.id.txt_profile_fullname);
         textViewPassword = ((ProfileActivity) context).findViewById(R.id.txt_profile_password);
         textViewPhone = ((ProfileActivity) context).findViewById(R.id.txt_profile_phone);
@@ -38,7 +42,7 @@ public class Profile_Adapter {
         textViewAddress = ((ProfileActivity) context).findViewById(R.id.txt_profile_address);
 
 
-// Gọi hàm getUserInfor() từ UserManager sau khi đăng nhập thành công
+        // Gọi hàm getUserInfor() từ UserManager sau khi đăng nhập thành công
         userManager.getUserInfor(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
@@ -47,20 +51,17 @@ public class Profile_Adapter {
                     // Hiển thị thông tin người dùng lên giao diện
                      User user = response.body();
                     // Hiển thị thông tin người dùng lên các EditText
+                    textViewUsername.setText(user.getUsername());
                     textViewFullname.setText(user.getFullname());
                     textViewPassword.setText(user.getPassword());
                     textViewPhone.setText(user.getPhone());
-                    // Chuyển đổi thuộc tính birth từ kiểu Date thành chuỗi định dạng ngày tháng
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
-                    String birthDateString = dateFormat.format(user.getBirth());
-                    textViewBirth.setText(birthDateString);
+                    textViewBirth.setText(user.getBirth());
                     textViewAddress.setText(user.getAddress());
 
                 } else {
                     Toast.makeText(context, "Lấy thông tin cá nhân thất bại!", Toast.LENGTH_SHORT).show();
                 }
             }
-
             @Override
             public void onFailure(Call<User> call, Throwable t) {
                 Toast.makeText(context, "Lấy thông tin cá nhân thất bại: " + t.getMessage(), Toast.LENGTH_SHORT).show();
@@ -69,4 +70,49 @@ public class Profile_Adapter {
 
 
     }
+
+    public void updateUserInfor() {
+        // Lấy thông tin mới từ EditText
+        String username = textViewUsername.getText().toString();
+        String fullname = textViewFullname.getText().toString();
+        String password = textViewPassword.getText().toString();
+        String phone = textViewPhone.getText().toString();
+        String birth = textViewBirth.getText().toString();
+        String address = textViewAddress.getText().toString();
+
+        // Kiểm tra xem ngày sinh có đúng định dạng không
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+        dateFormat.setLenient(false); // Đặt cờ để kiểm tra ngày tháng năm chính xác
+
+        Date birthDate;
+        try {
+            birthDate = dateFormat.parse(birth);
+        } catch (ParseException e) {
+            Toast.makeText(context, "Vui lòng nhập ngày sinh đúng định dạng \"dd-MM-yyyy\"", Toast.LENGTH_SHORT).show();
+            return; // Dừng hàm nếu ngày sinh không đúng định dạng
+        }
+
+        // Tạo user mới với thông tin mới
+        User updatedUser = new User(username, password, fullname, phone, address, "", dateFormat.format(birthDate), "");
+
+        // Gọi phương thức updateUserInfor từ UserManager
+        userManager.updateUserInfor(updatedUser, new Callback<Response>() {
+            @Override
+            public void onResponse(Call<Response> call, Response<Response> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    // Xử lý khi cập nhật thành công
+                    Toast.makeText(context, "Cập nhật thông tin thành công!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "Cập nhật thất bại!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Response> call, Throwable t) {
+                // Xử lý khi gặp lỗi trong quá trình cập nhật
+                Toast.makeText(context, "Cập nhật thất bại: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
